342f724e1974ee6c4e40b6d484c44ceb29f1a0d1
herrxyz <herrxyz@users.noreply.github.com>
2024-12-02 21:56:04 +0100

    Update const.py
    added circulation mode

--- custom_components/solvis_modbus/const.py
+++ custom_components/solvis_modbus/const.py
@@ -129,4 +129,11 @@ REGISTERS = [
         device_class="speed",
         state_class="measurement",
     ),
+    ModbusFieldConfig(
+        name="circulation_mode",
+        address=2049,
+        unit="",
+        device_class= None,
+        state_class="state",
+    ),
 ]
